2fc6caa603ae79cee6219b1ded7ab0ef47dca080
Evgeny Koshelev <eko2000@gmail.com>
2026-07-11 19:43:40 +0200

    Remove .next

--- frontend/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"yZ8Mck82hG1DAXLdhJknsLaoofsvTBLCtjav0G5F9TQ=\"\n}"
--- frontend/.next/dev/static/chunks/src_app_page_tsx_004tgyl._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_1jdswar._.js","static/chunks/src_components_13slm95._.js","static/chunks/node_modules_react-day-picker_src_style_03pf1y4.css"],
-    source: "dynamic"
-});
